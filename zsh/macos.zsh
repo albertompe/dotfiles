6e51c2ab76b0faa5ea7aee15454d2d611140f4b3
Alberto Martínez Pérez <106817741+albertompe@users.noreply.github.com>
2025-12-19 21:32:54 +0100

    Remove git() function, not needed if using pinentry-mac

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,9 +1,1 @@
 # macOS specific settings
-
-# Set GPG_TTY to enable gpg-agent support for pinentry
-git() {
-    if [[ "$1" == "commit" || "$1" == "tag" ]]; then
-        export GPG_TTY=$(tty)
-    fi
-    command git "$@"
-}
