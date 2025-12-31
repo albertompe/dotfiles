c45463b4980acac92824ef1a821d008d3ebd3e46
Alberto Martínez Pérez <106817741+albertompe@users.noreply.github.com>
2025-12-31 17:17:46 +0000

    Use zinit as zsh plugin manager, configuration and cleaning up submodules

--- zsh/zshrc-custom.zsh
+++ zsh/zshrc-custom.zsh
@@ -9,6 +9,38 @@ else
   export GPG_TTY="$TTY"
 fi
 
+# Path to the dotfiles.
+export DOTFILES="$HOME/.dotfiles"
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+autoload -Uz compinit
+compinit
+
+# Initialize zinit, downloading it if it's not done yet
+ZINIT_HOME="${XDG_DATA_HOME:-${DOTFILES}}/zinit/zinit.git"
+if [ ! -d "$ZINIT_HOME" ]; then
+   mkdir -p "$(dirname $ZINIT_HOME)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source "${ZINIT_HOME}/zinit.zsh"
+
+zinit light ohmyzsh/ohmyzsh
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-syntax-highlighting
+
+# Load Powerlevel10k theme
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+source $DOTFILES/zsh/themes/p10k-lean.zsh
+
 # Aliases definition
 source $DOTFILES/zsh/aliases.zsh
 
