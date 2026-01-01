dfec4e3bed9035c9493c93bcb48863a968e9e889
Alberto Martínez Pérez <alb.mtez@gmail.com>
2026-01-01 22:41:35 +0000

    Adding .zshenv dotfile, configuring Wezterm theme by environement variable and refactoring

---
+++ zsh/zshenv.zsh
@@ -0,0 +1,14 @@
+# Sets the GPG_TTY to be the same as the TTY, required to enter
+# GPG passphrases in a terminal
+if [ -n "$TTY" ]; then
+  export GPG_TTY=$(tty)
+else
+  export GPG_TTY="$TTY"
+fi
+
+# Wezterm theme (onedark or nord)
+export WEZTERM_THEME="onedark"
+
+# Use Neovim as default editor
+export EDITOR="nvim"
+export VISUAL="nvim"
