3e3ec6aa9d853e4dd363bdcb0468465093c4088a
Alberto Martínez Pérez <106817741+albertompe@users.noreply.github.com>
2026-01-01 01:50:25 +0000

    Adding a new function to update the system tools

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,1 +1,9 @@
 # Linux specific settings
+
+# Update system tools function
+system-update() {
+  echo "🛠️ Updating system tools..."
+  sudo apt update && sudo apt upgrade -y
+  zinit-update
+  echo "🎉 Everything's fresh and clean!"
+}
