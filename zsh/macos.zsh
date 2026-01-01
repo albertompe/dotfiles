9018c7fcad9b9e0a0e15b3c971a38dd7172cc2cd
Alberto Martínez Pérez <alb.mtez@gmail.com>
2026-01-01 01:50:25 +0000

    Adding a new function to update the system tools

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,1 +1,9 @@
 # macOS specific settings
+
+# Update system tools function
+system-update() {
+  echo "🛠️ Updating system tools..."
+  brew update && brew upgrade
+  zinit-update
+  echo "🎉 Everything's fresh and clean!"
+}
