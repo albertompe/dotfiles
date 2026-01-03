366d3e6a1462b01398e7472c77ba499a6398c076
Alberto Martínez Pérez <alb.mtez@gmail.com>
2026-01-03 02:42:29 +0000

    Add eza aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,3 +11,14 @@ alias ll='ls -lh'
 alias k=kubectl
 alias kcc=kubectx
 alias kns=kubens
+
+# eza aliases
+alias eza='eza -g --icons'
+alias ezal='eza -lah -a --icons'
+alias ezas='eza -lah -a --icons --sort=size'
+alias ezam='eza -lah -a --icons --sort=modified'
+alias ezae='eza -lah -a --icons --sort=extension'
+alias ezag='eza -lah -a --icons --git'
+alias ezad='eza -lah -a --icons --only-dirs'
+alias ezar='eza -lah -a --icons --reverse'
+alias ezatree='eza --icons --tree'
