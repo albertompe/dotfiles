b83095d02e6bc532a0760c8a92332624d79e78b7
Alberto Martínez Pérez <alb.mtez@gmail.com>
2026-01-03 02:05:48 +0000

    Use Starship or Oh my zsh + powerlevel10k, using a new environment variable

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -6,10 +6,16 @@ else
   export GPG_TTY="$TTY"
 fi
 
-# Wezterm theme (onedark or nord)
-export WEZTERM_THEME="onedark"
-# Starship theme (onedark or nord)
-export STARSHIP_THEME="onedark"
+# Select prompt: "p10k" or "starship"
+export SELECTED_PROMPT="starship"
+
+# Global theme: "onedark" or "nord"
+export DOTFILES_THEME="onedark"
+
+# Wezterm theme. DOTFILES_THEME set by default. You can override it here if needed: "onedark" or "nord"
+export WEZTERM_THEME=$DOTFILES_THEME
+# Starship theme. DOTFILES_THEME set by default. You can override it here if needed: "onedark" or "nord"
+export STARSHIP_THEME=$DOTFILES_THEME
 
 # Use Neovim as default editor
 export EDITOR="nvim"
