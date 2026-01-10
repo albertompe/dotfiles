77fdbc0e49faddf6e96902abe9268a6d79c56ebc
Alberto Martínez Pérez <106817741+albertompe@users.noreply.github.com>
2026-01-10 00:12:12 +0000

    Add keybindings to go to the beginning and end of the line

---
+++ zsh/keybindings.zsh
@@ -0,0 +1,4 @@
+# Navigate to the beginning and end of the line using Home and End keys
+# (Fn + Left Arrow and Fn + Right Arrow on macOS keyboards)
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
