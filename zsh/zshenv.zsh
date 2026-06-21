0ff99db754ebeaed4eb175076864f03f7961344c
Alberto Martínez Pérez <106817741+albertompe@users.noreply.github.com>
2026-06-21 18:12:47 +0200

    Formatting and set nord as default theme

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,9 +1,9 @@
 # Sets the GPG_TTY to be the same as the TTY, required to enter
 # GPG passphrases in a terminal
 if [ -n "$TTY" ]; then
-  export GPG_TTY=$(tty)
+    export GPG_TTY=$(tty)
 else
-  export GPG_TTY="$TTY"
+    export GPG_TTY="$TTY"
 fi
 
 # Select prompt:
@@ -13,7 +13,7 @@ fi
 export SELECTED_PROMPT="omp"
 
 # Global theme: "onedark" or "nord"
-export DOTFILES_THEME="onedark"
+export DOTFILES_THEME="nord"
 
 # Wezterm theme. DOTFILES_THEME set by default. You can override it here if needed: "onedark" or "nord"
 export WEZTERM_THEME=$DOTFILES_THEME
@@ -22,12 +22,12 @@ export STARSHIP_THEME=$DOTFILES_THEME
 
 # Set the default editor (Zed if installed and not in a ssh connection, nvim otherwise)
 if [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_CLIENT" ]; then
-  export EDITOR="nvim"
-  export VISUAL="nvim"
+    export EDITOR="nvim"
+    export VISUAL="nvim"
 elif [ -x "/opt/homebrew/bin/zed" ] || [ -x "$HOME/.local/bin/zed" ] || [ -x "/usr/local/bin/zed" ]; then
-  export VISUAL="zed --new --wait"
-  export EDITOR="zed --new --wait"
+    export VISUAL="zed --new --wait"
+    export EDITOR="zed --new --wait"
 else
-  export EDITOR="nvim"
-  export VISUAL="nvim"
+    export EDITOR="nvim"
+    export VISUAL="nvim"
 fi
